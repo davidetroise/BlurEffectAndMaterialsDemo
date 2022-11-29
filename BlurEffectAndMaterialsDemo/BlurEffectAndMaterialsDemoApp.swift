05360a0b94f2c1264d8d19d1a0d7d994221b57e2
Davide Troise <davide@troise.net>
2022-11-29 11:27:21 +0100

    Initial Commit

---
+++ BlurEffectAndMaterialsDemo/BlurEffectAndMaterialsDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  BlurEffectAndMaterialsDemoApp.swift
+//  BlurEffectAndMaterialsDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct BlurEffectAndMaterialsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
